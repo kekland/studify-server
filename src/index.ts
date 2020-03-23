9e4fb79ad2edb28b88da69e09e157817c28cf1cd
Erzhan <kk.erzhan@gmail.com>
2020-03-23 15:35:37 +0600

    impl: added routers to express

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,34 @@
 import { configuration } from "./config"
 import { MongoClient } from "mongodb"
 import { RepositoryManager } from "./database/database"
+import { authRouter } from "./router/auth"
+import express from 'express'
+import bodyParser from 'body-parser'
 
 const bootstrap = async () => {
-  const uri = configuration.uri
-  const client = new MongoClient(uri, { useNewUrlParser: true })
+  // Load configuration
+  const {port, uri} = configuration
 
+  // Load Express
+  const app = express()
+  app.use(bodyParser())
+
+  // Connect to mongo
+  const client = new MongoClient(uri, { useNewUrlParser: true })
   await client.connect()
 
+  // Initialize repositories
   RepositoryManager.initialize(client)
-  console.log('Done!')
+  
+  // Setup routers
+  const routers = [
+    authRouter(),
+  ]
+
+  routers.forEach(router => app.use(router))
+
+  // Start server on port
+  app.listen(port)
 }
 
 bootstrap()
