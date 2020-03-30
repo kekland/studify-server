ea59e6739af164d5580d8e8e0e42deb39db3327d
Erzhan <kk.erzhan@gmail.com>
2020-03-30 15:37:00 +0600

    wip: file upload

--- src/index.ts
+++ src/index.ts
@@ -13,16 +13,40 @@ import cors from 'cors'
 import http from 'http'
 import { notificationsRouter } from "./router/notifications";
 import { usersRouter } from "./router/user";
+import fileUpload from 'express-fileupload'
+import admin from 'firebase-admin'
 
 let isLocal = process.env.PORT == null
 let config = Local.configuration
-if (!isLocal)
-  config = Heroku.configuration
+let firebaseAdminCredentials: any
 
+if (!isLocal) {
+  config = Heroku.configuration
+  firebaseAdminCredentials = {
+    "type": process.env.FIREBASE_TYPE,
+    "project_id": process.env.FIREBASE_PROJECT_ID,
+    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY,
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    "client_id": process.env.FIREBASE_CLIENT_ID,
+    "auth_uri": process.env.FIREBASE_AUTH_URI,
+    "token_uri": process.env.FIREBASE_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  }
+}
+else {
+  firebaseAdminCredentials = require(__dirname.replace('build', 'firebase-admin.json'))
+}
 const bootstrap = async () => {
   Logging.info('Bootstrap', 'Starting server')
   // Load configuration
   const { port, options } = config
+
+  // Load firebase
+  admin.initializeApp({
+    credential: admin.credential.cert(firebaseAdminCredentials)
+  })
 
   // Load server
   const expressServer = express()
@@ -30,6 +54,7 @@ const bootstrap = async () => {
 
   expressServer.use(cors())
   expressServer.use(bodyParser({ extended: true }))
+  expressServer.use(fileUpload({ abortOnLimit: true, parseNested: true, limits: { fileSize: 10 * 1024 * 1024 } }))
 
   if (isLocal) {
     expressServer.use((req, res, next) => {
